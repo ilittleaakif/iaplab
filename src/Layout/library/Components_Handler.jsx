795f7e0559c0ddc6f0b138507af844e4d943b7c8
Zakaria iouiri <ziwiri75@gmail.com>
2025-09-21 19:03:59 +0100

    edited UI

--- src/Layout/library/Components_Handler.jsx
+++ src/Layout/library/Components_Handler.jsx
@@ -1,103 +1,6 @@
-import { CalendarRange, ArrowRight, Layers, Youtube } from "lucide-react";
-
+import { CalendarRange, ArrowRight, Layers, Youtube, Grid2X2Plus, ListVideo } from "lucide-react";
 const OpenLink = (link) => window.open(link, "_blank");
 
-
-
-export const ExtraCard = ({ title, icon: Icon, Link }) => {
-  return (
-    <div
-      onClick={() => OpenLink(Link)}
-      className="text-accent gap-3 border-2 border-accent p-6 flex flex-col items-center justify-center hover:-translate-y-1 active:translate-y-1 cursor-pointer transition-all">
-      <Icon className="w-8 h-8 " />
-      <div className="font-bold text-text-soft text-center text-md">{title}</div>
-    </div>
-  );
-};
-
-export const ModuleCard = ({ title, icon: Icon, Link }) => {
-  return (
-    <div
-      onClick={() => OpenLink(Link)}
-      className="text-text gap-3 border-2 border-text p-6 flex flex-col items-center justify-center hover:-translate-y-1 active:translate-y-1 cursor-pointer transition-all">
-      <Icon className="w-8 h-8 " />
-      <div className="font-bold text-center text-md">{title}</div>
-    </div>
-  );
-};
-
-export const YoutubeCard = ({ title, Link }) => {
-  return (
-    <div
-      onClick={() => OpenLink(Link)}
-      className="text-youtube gap-3 border-2 border-youtube p-6 flex flex-col items-center justify-center hover:-translate-y-1 active:translate-y-1 cursor-pointer transition-all">
-      <Youtube className="w-8 h-8" />
-      <div className="font-bold text-center text-md">{title}</div>
-    </div>
-  );
-};
-
-
-
-
-
-export const SemesterContainer = ({ extras, youtube, modules, sectionRef }) => {
-  return (
-    <section ref={sectionRef} className="md:p-6 p-4 space-y-8 text-text">
-
-      {/* Modules */}
-      <>
-        <div className="flex items-center gap-3 pb-3 border-b">
-          <CalendarRange />
-          <h2 className="text-lg font-bold">Modules</h2>
-        </div>
-
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          {modules.map((mod, i) => (
-            <ModuleCard key={i} title={mod.title} icon={mod.icon} Link={mod.Url} />
-          ))}
-        </div>
-      </>
-
-      {/* Youtubes */}
-      {youtube?.length > 0 &&
-        <>
-          <div className="flex items-center justify-between pb-3 border-b">
-            <div className="flex items-center gap-3">
-              <Layers />
-              <h2 className="text-lg font-bold">Youtube Playlists</h2>
-            </div>
-          </div>
-
-          <div className="grid items-center w-full grid-cols-1 gap-4 transition-all md:grid-cols-3">
-            {youtube.map((yt, i) => (
-              <YoutubeCard key={i} Link={yt.Url} title={yt.title} icon={yt.icon} />
-            ))}
-          </div>
-        </>
-      }
-
-      {/* Extras */}
-      {extras?.length > 0 &&
-        <>
-          <div className="flex items-center justify-between pb-3 border-b">
-            <div className="flex items-center gap-3">
-              <Layers />
-              <h2 className="text-lg font-bold">Pictures</h2>
-            </div>
-          </div>
-
-
-          <div className="grid md:grid-cols-4 grid-cols-2 gap-2">
-            {extras.map((extra, i) => (
-              <ExtraCard key={i} Link={extra.Url} title={extra.title} icon={extra.icon} />
-            ))}
-          </div>
-        </>
-      }
-    </section>
-  );
-};
 
 export const UnivCard = ({ icon: Icon, title, des, btn, Link, clickEvent = null }) => {
   return (
@@ -119,3 +22,99 @@ export const UnivCard = ({ icon: Icon, title, des, btn, Link, clickEvent = null 
     </div>
   )
 }
+
+export const ExtraCard = ({ title, icon: Icon, Link }) => {
+  return (
+    <div
+      onClick={() => OpenLink(Link)}
+      className="text-accent gap-3 border-2 border-accent p-4  md:p-6 flex md:flex-col items-center md:justify-center hover:-translate-y-1 active:translate-y-1 cursor-pointer transition-all">
+      <Icon className="md:w-8 md:h-8 w-5 h-5 " />
+      <div className="font-bold text-text-soft text-center text-md">{title}</div>
+    </div>
+  );
+};
+
+export const ModuleCard = ({ title, icon: Icon, Link }) => {
+  return (
+    <div
+      onClick={() => OpenLink(Link)}
+      className="text-text gap-3 border-2 border-text md:p-6 p-4 flex md:flex-col items-center md:justify-center hover:-translate-y-1 active:translate-y-1 cursor-pointer transition-all">
+      <Icon className="w-5 h-5 md:w-8 md:h-8 " />
+      <div className="font-bold text-center text-sm md:text-md">{title}</div>
+    </div>
+  );
+};
+
+export const YoutubeCard = ({ title, Link }) => {
+  return (
+    <div
+      onClick={() => OpenLink(Link)}
+      className="text-youtube gap-3 border-2 border-youtube md:p-6 p-4  flex md:flex-col items-center md:justify-center hover:-translate-y-1 active:translate-y-1 cursor-pointer transition-all">
+      <Youtube className="w-5 h-5 md:w-8 md:h-8 " />
+      <div className="font-bold text-center text-sm md:text-md">{title}</div>
+    </div>
+  );
+};
+
+
+
+
+
+export const SemesterContainer = ({ extras, youtube, modules, sectionRef }) => {
+  return (
+    <section ref={sectionRef} className="md:p-6 p-2 space-y-8 text-text">
+
+      {/* Modules */}
+      <div className="space-y-3">
+        <div className="flex items-center gap-3 pb-3 border-b">
+          <CalendarRange className="h-5 w-5 md:h-6 md:w-6" />
+          <h2 className="text-lg font-bold">Modules</h2>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {modules.map((mod, i) => (
+            <ModuleCard key={i} title={mod.title} icon={mod.icon} Link={mod.Url} />
+          ))}
+        </div>
+      </div>
+
+      {/* Youtubes */}
+      {youtube?.length > 0 &&
+        <div className="space-y-3">
+          <div className="flex items-center justify-between pb-3 border-b">
+            <div className="flex items-center gap-3">
+              <ListVideo className="h-5 w-5 md:h-6 md:w-6" />
+              <h2 className="text-lg font-bold">Youtube Playlists</h2>
+            </div>
+          </div>
+
+          <div className="grid items-center w-full grid-cols-1 gap-4 transition-all md:grid-cols-3">
+            {youtube.map((yt, i) => (
+              <YoutubeCard key={i} Link={yt.Url} title={yt.title} icon={yt.icon} />
+            ))}
+          </div>
+        </div>
+      }
+
+      {/* Extras */}
+      {extras?.length > 0 &&
+        <div div className="space-y-3">
+          <div className="flex items-center justify-between pb-3 border-b">
+            <div className="flex items-center gap-3">
+              <Grid2X2Plus className="h-5 w-5 md:h-6 md:w-6" />
+              <h2 className="text-lg font-bold">Pictures</h2>
+            </div>
+          </div>
+
+
+          <div className="grid md:grid-cols-4 grid-cols-1 gap-2">
+            {extras.map((extra, i) => (
+              <ExtraCard key={i} Link={extra.Url} title={extra.title} icon={extra.icon} />
+            ))}
+          </div>
+        </div>
+      }
+    </section>
+  );
+};
+
