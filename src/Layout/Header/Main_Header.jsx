f3ca4c521230b2a86814cb9ff216d3fe1a387b45
Zakaria iouiri <ziwiri75@gmail.com>
2025-10-02 18:03:47 +0100

    .

--- src/Layout/Header/Main_Header.jsx
+++ src/Layout/Header/Main_Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
   useEffect(() => {
     const typed = new Typed(TypedRef.current, {
-      strings: ['<i class="fa-solid fa-dove"></i>حِدَاد'],
+      strings: ['<i class="fa-solid fa-dove"></i>حِــــــــــــــــــــــدَاد'],
       typeSpeed: 80,
       cursorChar: ''
     });
@@ -31,8 +31,7 @@ function Header() {
       {/* logo */}
       <div className='relative'>
         <span className="font-bold animate-fadein md:text-4xl text-3xl relative font-logo transition bg-gradient-to-r from-text to-text-soft  bg-clip-text text-transparent">IAPLAB</span>
-        <p ref={TypedRef} className='font-quran font-bold text-youtube md:text-[18px] animate-pulse text-[16px] absolute left-1/2 -translate-x-1/2 md:-bottom-[10px] -bottom-[6px] block whitespace-nowrap'>
-        </p>
+        <p ref={TypedRef} className='font-quran font-bold text-youtube md:text-[18px] animate-pulse text-[16px] absolute left-1/2 -translate-x-1/2 md:-bottom-[10px] -bottom-[6px] block whitespace-nowrap'></p>
       </div>
     </header >
   );
